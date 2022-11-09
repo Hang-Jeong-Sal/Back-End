bdca08cdffc37d8b26eddc1b575a1534ae47b939
math0917 <math0917@naver.com>
2022-11-09 21:43:45 +0900

    feat: 생성 프로젝트

---
+++ src/main/java/field/platform/PlatformApplication.java
@@ -0,0 +1,21 @@
+package field.platform;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import lombok.Getter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
+}
