01ca7c2bba76306d82bafb1c00ff3bbaa941c7a4
math0917 <math0917@naver.com>
2022-11-29 19:26:08 +0900

    feat(likeService): 좋아요 기능 추가

--- src/main/java/field/platform/controller/LikeController.java
+++ src/main/java/field/platform/controller/LikeController.java
@@ -1,24 +1,24 @@
-//package field.platform.controller;
-//
-//import field.platform.service.LikeService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PatchMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestHeader;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@Slf4j
-//public class LikeController {
-//
-//    private final LikeService likeService;
-//
-//    @PatchMapping("api/likes/{ground_id}")
-//    public ResponseEntity changeLikeStatus(@PathVariable(name = "ground_id") Long groundId,
-//                                           @RequestHeader(name = "access_token") String accessToken) {
-//        return likeService.changeLikeStatus(groundId,accessToken);
-//    }
-//}
+package field.platform.controller;
+
+import field.platform.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @PatchMapping("api/likes/{ground_id}")
+    public ResponseEntity changeLikeStatus(@PathVariable(name = "ground_id") Long groundId,
+                                           @RequestHeader(name = "Authorization") String accessToken) {
+        return likeService.changeLikeStatus(groundId,accessToken);
+    }
+}
